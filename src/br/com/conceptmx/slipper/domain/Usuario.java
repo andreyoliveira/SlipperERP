39294fd3edba42fe957f20ddfd3f18b7b79ff001
andreyoliveira <andreyoliveira@192.168.0.100>
2014-01-13 22:21:21 -0200

    Criação da classe Usuario.

---
+++ src/br/com/conceptmx/slipper/domain/Usuario.java
@@ -0,0 +1,49 @@
+package br.com.conceptmx.slipper.domain;
+
+/**
+ * @author andreyoliveira
+ * @since 13/01/2014
+ */
+public class Usuario {
+	
+	private Long id;
+	
+	private String nome;
+	
+	private String username;
+	
+	private String password;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}

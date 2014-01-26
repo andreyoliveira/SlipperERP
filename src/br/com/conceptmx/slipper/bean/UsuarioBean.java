108882a42dff9168a24e618ce201ce5e90fa7270
andreyoliveira <andreyoliveira@192.168.0.100>
2014-01-26 17:12:41 -0200

    Inclusão do algoritmo de criptografia para senhas.

--- src/br/com/conceptmx/slipper/bean/UsuarioBean.java
+++ src/br/com/conceptmx/slipper/bean/UsuarioBean.java
@@ -1,5 +1,9 @@
 package br.com.conceptmx.slipper.bean;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.faces.bean.ManagedBean;
 
 import br.com.conceptmx.slipper.dao.DAO;
@@ -18,9 +22,25 @@ public class UsuarioBean {
 		return usuario;
 	}
 	
-	public String gravarUsuario(){
+	public String gravarUsuario() throws NoSuchAlgorithmException, UnsupportedEncodingException{
+		
+		criptografaSenha();
+		
 		new DAO<Usuario>(Usuario.class).adiciona(this.usuario);
 		return "index?faces-redirect=true";
+	}
+	
+	private void criptografaSenha() throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest algorythm = MessageDigest.getInstance("MD5");
+		byte[] messageDigest = algorythm.digest(this.usuario.getPassword().getBytes("UTF-8"));
+		
+		StringBuilder hexString = new StringBuilder();
+		for (byte b : messageDigest) {
+		  hexString.append(String.format("%02X", 0xFF & b));
+		}
+		String senha = hexString.toString();
+		
+		this.usuario.setPassword(senha);
 	}
 
 }
